4ddfde5ea712a4cf27cb786b452815a406fd7e25
SHAURYAGUPTA01 <86559522+SHAURYAGUPTA01@users.noreply.github.com>
2022-04-30 13:10:53 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+operation=input("choose a operation : ")
+print(operation)
+if(operation=="multiplication"):
+    
+    firstnumber = int(input("Enter a number : "))
+    secondnumber = int(input("Enter second number :"))
+    print(firstnumber*secondnumber)
+    
+elif(operation=="add"):
+    
+    firstnumber = int(input("Enter a number : "))
+    secondnumber = int(input("Enter second number : "))
+    print(firstnumber+secondnumber) 
+    
+elif(operation=="subtraction"):
+    
+    firstnumber = int(input("Enter a number : "))
+    secondnumber = int(input("Enter second number :"))
+    print(firstnumber-secondnumber)
+    
+elif(operation=="division"):
+    
+    firstnumber = int(input("Enter a number : "))
+    secondnumber = int(input("Enter second number : "))
+    print(firstnumber/secondnumber)
